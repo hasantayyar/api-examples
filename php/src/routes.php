e56cec439c1acb01d71fec25e21d508e6c9a521d
Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
2017-06-15 16:36:25 +0200

    cleanup

--- php/src/routes.php
+++ php/src/routes.php
@@ -1,16 +1,29 @@
 <?php
 // Routes
 
+$endPoint = "https://sandbox.bitwala.io";
+
+function get($path){
+
+}
+function post($path, $options){
+  
+} 
+
 $app->get('/', function ($request, $response, $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
 });
 
-$app->get('/{name}', function ($request, $response, $args) {
+$app->get('/info', function ($request, $response, $args) {
     return $this->renderer->render($response, 'info.phtml', $args);
-});	
-$app->get('/[{name}]', function ($request, $response, $args) {
-  // Sample log message
-  $this->logger->info("Slim-Skeleton '/' route");
-  // Render index view
-  return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->get('/inputs', function ($request, $response, $args) {
+    $data = get($endPoint.'/');
+    return $response->withJson($data);
+});
+
+$app->get('/outputs', function ($request, $response, $args) {
+
+    return $response->withJson($data);
 });
